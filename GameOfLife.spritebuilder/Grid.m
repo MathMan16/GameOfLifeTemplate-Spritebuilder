187d9553f37e6bcc0fed90a280c3a071b504cff2
Henry Thaman <hthaman@mit.edu>
2014-07-01 15:07:53 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -9,6 +9,8 @@ static const int GRID_COLUMNS = 10;
     NSMutableArray *_gridArray;
     float _cellWidth;
     float _cellHeight;
+    int _row;
+    int _column;
 }
 - (void)onEnter
 {
@@ -69,4 +71,13 @@ static const int GRID_COLUMNS = 10;
     //invert it's state - kill it if it's alive, bring it to life if it's dead.
     creature.isAlive = !creature.isAlive;
 }
+
+- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
+{
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    _row = touchPosition.y/_cellHeight;
+    _column = touchPosition.x/_cellWidth;
+    return _gridArray[_row][_column];
+}
+
 @end
