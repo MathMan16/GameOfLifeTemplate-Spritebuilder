26747f854bf0d708f65cf98862576bc05e9dd7fe
Henry Thaman <hthaman@mit.edu>
2014-07-02 04:52:37 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -38,4 +38,16 @@
     _populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
 }
 
+
+- (void)evolveStep
+{
+    //update each Creature's neighbor count
+    [self countNeighbors];
+    
+    //update each Creature's state
+    [self updateCreatures];
+    
+    //update the generation so the label's text will display the correct generation
+    _generation++;
+}
 @end
