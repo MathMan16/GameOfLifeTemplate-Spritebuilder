ac5ee45704a8ad5c60d7d61b5aa857f68789043e
Henry Thaman <hthaman@mit.edu>
2014-07-02 14:00:42 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -94,7 +94,7 @@ static const int GRID_COLUMNS = 10;
 
 - (void)updateCreatures
 {
-    int numCreatures = 0;
+//    int numCreatures = 0;
     for (int i = 0; i < [_gridArray count]; i++)
     {
         for (int j = 0; j < [_gridArray[i] count]; j++)
@@ -103,7 +103,7 @@ static const int GRID_COLUMNS = 10;
             if (currentCreature.livingNeighbors == 3)
             {
                 currentCreature.isAlive = TRUE;
-                numCreatures++;
+//                numCreatures++;
             }
             else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4)
             {
